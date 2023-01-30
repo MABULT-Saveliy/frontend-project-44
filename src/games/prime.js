2beae30358a99a37e21696d44a3291ddc4cba7a3
MABULT-Saveliy <123162874+MABULT-Saveliy@users.noreply.github.com>
2023-01-30 19:24:03 +0300

    Add files via upload

---
+++ src/games/prime.js
@@ -0,0 +1,20 @@
+const questions = Array.from({ length: 3 }, () => [Math.floor(Math.random() * 98 + 2)]);
+const rightAnswers = [];
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+for (let i = 0; i < 3; i += 1) {
+  let flag = true;
+  for (let j = 2; j <= Math.sqrt(questions[i]); j += 1) {
+    if (questions[i] % j === 0) {
+      // eslint-disable-next-line no-const-assign
+      flag = false;
+      break;
+    }
+  }
+  if (flag) {
+    rightAnswers.push('yes');
+  } else {
+    rightAnswers.push('no');
+  }
+}
+
+export { questions, rightAnswers, rules };
